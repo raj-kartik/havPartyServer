ac6df039f99774d1ac3d1a6d5ceb0bc9d5abab56
raj-kartik <rajkartik159@gmail.com>
2024-09-05 16:46:58 +0530

    club schema is partial done

--- models/clubSchema.js
+++ models/clubSchema.js
@@ -1,26 +1,51 @@
 import mongoose from "mongoose";
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const ClubSchema = new Schema({
-    name:{
-        type:String,
-        require:true
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    required: true,
+    type: String,
+  },
+  city: {
+    required: true,
+    type: String,
+  },
+  state: {
+    required: true,
+    type: String,
+  },
+  pincode: {
+    type: Number,
+    required: true,
+  },
+//   photos: [],
+  offers: [
+    {
+      offer: {
+        type: String
+      },
     },
-    location:{
-        require:true,
-        type:String
-    },
-    city:{
-        require:true,
-        type:String
-    },
-    state:{
-        require:true,
-        type:String
-    },
-    pincode:{
-        type:Number,
-        require:true
+  ],
+  price:[
+    {
+        single:{
+            type:Number,
+            require:true
+        },
+        couple:{
+            type:Number,
+            require:true
+        },
     }
-})
-// hello i am in master
+  ],
+  menu:{
+    drink:[],
+    foods:[],
+  }
+});
+
+export default mongoose.model('Club', ClubSchema);
