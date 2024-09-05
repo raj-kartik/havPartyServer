2fe134c8ec09f197df6c3aabe8582b38ac8143fd
raj-kartik <rajkartik159@gmail.com>
2024-09-06 03:45:39 +0530

    express changed

--- index.js
+++ index.js
@@ -1,27 +1,16 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import userRoute from './routes/user.js';
-import mongoose from 'mongoose';
+const express = require('express');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const PORT = 8000
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
-console.log('MONGO_URL:', process.env.MONGO_URL);
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
 
-mongoose.connect(process.env.MONGO_URL)
-.then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('MongoDB connection error:', error));
-
-// Define routes
-app.get("/", (req, res) => {
-    res.send("I have connected to vercel");
-});
-
-app.use('/auth', userRoute);
-
-app.listen(port, () => {
-    console.log("-- listening on PORT", port);
-});
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+})
