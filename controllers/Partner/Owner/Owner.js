135efb717f2510687108b26c3ea450c2d8ebacb4
raj-kartik <rajkartik159@gmail.com>
2025-03-30 23:20:38 +0530

    owner login url added

--- controllers/Partner/Owner/Owner.js
+++ controllers/Partner/Owner/Owner.js
@@ -1,6 +1,7 @@
 // import Owner from "../../../models/Partner/Owner/owner.js";
 import Owner from "../../../models/Owner/owner.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 export const ownerSignUp = async (req, res) => {
   const { name, email, mobile, password } = req.body;
 
@@ -21,7 +22,7 @@ export const ownerSignUp = async (req, res) => {
     const owner = new Owner({
       name,
       email,
-      contact_number:mobile,
+      contact_number: mobile,
       password: hashedPassword,
     });
 
@@ -34,7 +35,50 @@ export const ownerSignUp = async (req, res) => {
         mobile: mobile,
         email: email,
       },
-      status:200
+      status: 200,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const ownerSignIn = async (req, res) => {
+  const { mobile, password } = req.body;
+
+  
+
+  if ((!mobile, !password))
+    return res.status(500).json({
+      message: "All fields are required",
+    });
+
+  try {
+    const user = await Owner.findOne({ contact_number: mobile });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+
+    if (user.isBlocked || user.isBanned) {
+      return res.status(403).json({ message: "User is blocked or banned." });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+
+    // Generate a never-expiring JWT token
+    const token = jwt.sign(
+      { ownerId: user._id, email: user?.email, mobile: user?.mobile },
+      process.env.JWT_SECRET // No 'expiresIn' option means the token will not expire
+    );
+
+    // Respond with user details and token
+    return res.status(200).json({
+      message: "Login successful.",
+      user,
+      token,
     });
   } catch (err) {
     console.error(err);
--- routes/Owner/owner.js
+++ routes/Owner/owner.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { ownerSignUp } from "../../controllers/Partner/Owner/Owner.js";
+import { ownerSignIn, ownerSignUp } from "../../controllers/Partner/Owner/Owner.js";
 import { getEmployee, getEmployeeDetails } from "../../controllers/Partner/PartnerController.js";
 
 const router = express.Router();
 
 // post
 router.post("/signup", ownerSignUp);
+router.post("/signin", ownerSignIn);
 // get
 router.get("/employees", getEmployee);
 // get 
